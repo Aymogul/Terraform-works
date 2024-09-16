c1a00f7967f2cbec54e5849471b398f78d3c955a
Aymogul <aymogul22@gmail.com>
2024-09-16 08:44:53 -0700

    usr data

---
+++ main4.tf
@@ -0,0 +1,10 @@
+resource "aws_launch_configuration" "example" {
+ image_id = "ami-0fb653ca2d3203ac1"
+ instance_type = "t2.micro"
+ security_groups = [aws_security_group.instance.id]
+ # Render the User Data script as a template
+ user_data = templatefile("user-data.sh", {
+ server_port = var.server_port
+ db_address = data.terraform_remote_state.db.outputs.address
+ db_port = data.terraform_remote_state.db.outputs.port
+ })
